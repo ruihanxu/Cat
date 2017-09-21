e317588328ddeaf843b571ea3137ff987d827d08
ruihanxu <xuruihan@lawn-128-61-48-181.lawn.gatech.edu>
2017-09-21 16:57:44 -0400

    3

---
+++ Cats/app/src/main/java/com/example/xuruihan/cats/model/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.xuruihan.cats.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xuruihan.cats.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
