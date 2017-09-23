8e40cceba3382318b999ce7d911596be0bfb7520
JLuo <jeffreyzcluo@gmail.com>
2017-09-22 23:09:17 -0400

    Press Logout button to go back to start page

--- Cats/app/src/main/java/com/example/xuruihan/cats/MapActivity.java
+++ Cats/app/src/main/java/com/example/xuruihan/cats/MapActivity.java
@@ -1,13 +1,25 @@
 package com.example.xuruihan.cats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MapActivity extends AppCompatActivity {
 
+    private Button logoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        logoutButton = (Button) findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener((View v) -> {
+            Intent intent = new Intent(this, StartUpActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
