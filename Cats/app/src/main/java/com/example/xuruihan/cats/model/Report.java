cb17bb92abd2362188cd371d46a6ea416c084fea
sophiasun0515 <ruixuansun@gatech.edu>
2017-10-06 16:38:39 -0400

    Report Added

---
+++ Cats/app/src/main/java/com/example/xuruihan/cats/model/Report.java
@@ -0,0 +1,8 @@
+package com.example.xuruihan.cats.model;
+
+/**
+ * Created by Ruixuan on 10/6/17.
+ */
+
+public class Report {
+}
---
+++ Cats/app/src/main/java/com/example/xuruihan/cats/util/ReportActivity.java
@@ -0,0 +1,8 @@
+package com.example.xuruihan.cats.util;
+
+/**
+ * Created by Ruixuan on 10/6/17.
+ */
+
+public class ReportActivity {
+}
