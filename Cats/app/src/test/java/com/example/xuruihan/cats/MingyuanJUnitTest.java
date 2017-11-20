cc0c0ad41f40b762b0f769de3370b8a0b5bc0770
cugg0614 <cugg.zmy@gatech.edu>
2017-11-19 22:09:25 -0500

    Mingyuan Zhou JUnit Test

---
+++ Cats/app/src/test/java/com/example/xuruihan/cats/MingyuanJUnitTest.java
@@ -0,0 +1,66 @@
+package com.example.xuruihan.cats;
+
+import com.example.xuruihan.cats.model.User;
+import com.example.xuruihan.cats.model.UserItem;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MingyuanJUnitTest {
+
+    private User user;
+    private UserItem userItem;
+    private static final int TIMEOUT = 200;
+
+    @Before
+    public void setup() {
+        userItem = new UserItem("50000000", "password", true);
+        user = new User(userItem);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testSetUser() {
+        UserItem expectedUserItem = new UserItem("555555555", "password", true);
+        User expectedUser = new User(expectedUserItem);
+        userItem.setUser("555555555");
+        user.setUser("555555555", "password");
+        assertEquals(expectedUserItem, userItem);
+        assertEquals(expectedUser, user);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testSetPassword() {
+        UserItem expectedUserItem = new UserItem("UserID", "asdfasdf", true);
+        User expectedUser = new User(expectedUserItem);
+        userItem.setPass("asdfasdf");
+        user.setUser("UserID", "asdfasdf");
+        assertEquals(expectedUserItem, userItem);
+        assertEquals(expectedUser, user);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testNullUserInfo() {
+        int count = 0;
+        try {
+            user.setUser(null,  null);
+        } catch (IllegalArgumentException e) {
+            count++;
+        }
+        assertEquals("There is null exception : ", 1, count);
+    }
+
+    @Test(timeout = TIMEOUT)
+    public void testSetEmptyUser() {
+        userItem = new UserItem("500000001", "password", true);
+        user = new User(userItem);
+        UserItem expectedUserItem = new UserItem("500000001", "password", true);
+        User expectedUser = new User(expectedUserItem);
+
+        userItem.setUser("");
+        user.setUser("", "");
+        assertEquals(expectedUser, user);
+    }
+}
